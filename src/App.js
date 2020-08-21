b398bfc2950995bf3ed963510c1b946a731f7d4b
bharat_patil <patilbharat09@gmail.com>
2020-08-21 14:11:38 +0530

    Added popup modal, shows machine info, Mobile app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Plugins } from "@capacitor/core";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+const { Device, Modals } = Plugins;
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.showDeviceInfo = this.showDeviceInfo.bind(this);
+  }
+
+  async showDeviceInfo() {
+    let info = await Device.getInfo();
+    await Modals.alert({
+      title: "Info",
+      message: `UUID: ${info.uuid};
+            Model: ${info.model}`,
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <button onClick={this.showDeviceInfo}> Show Device Info</button>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
